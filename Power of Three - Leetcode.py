8ca00d12353a4f5c91b29a75215d27215fefc7dc
NabilMShandy <nmuhammadshandy@gmail.com>
2026-03-31 18:53:38 +0700

    20 ribu tes case jir

---
+++ Power of Three - Leetcode.py
@@ -0,0 +1,36 @@
+# Given an integer n, return true if it is a power of three. Otherwise, return false.
+# An integer n is a power of three, if there exists an integer x such that n == 3^x
+
+# Example 1:
+# Input: n = 27
+# Output: true
+# Explanation: 27 = 3^3
+
+# Example 2:
+# Input: n = 0
+# Output: false
+# Explanation: There is no x where 3^x = 0.
+
+# Example 3:
+# Input: n = -1
+# Output: false
+# Explanation: There is no x where 3x = (-1).
+
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n == 0 or n == -1:
+            return False
+        if n == 1:
+            return True
+        counter = 0
+        saveN = n
+        hasil = None
+        while n > 0:
+            hasil = n//3
+            n = hasil
+            if hasil % 3 == 0 and hasil!= 0:
+                counter += 1
+        return 3**(counter+1) == saveN    
+
+tes = Solution()
+print(tes.isPowerOfThree(81))
