042afbf2487ea9c3208f4e25c152bf633e908c27
NabilMShandy <nmuhammadshandy@gmail.com>
2026-04-18 21:51:34 +0700

    fix dikit

--- Power of Three - Leetcode.py
+++ Power of Three - Leetcode.py
@@ -18,7 +18,7 @@
 
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
-        if n == 0 or n == -1:
+        if n <= 0:
             return False
         if n == 1:
             return True
